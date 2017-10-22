d4bfa27cc0fe842c0d81284d654fd930e051917f
Ronnie Isurina <ronnie.isurina@gmail.com>
2017-10-22 19:11:48 +0800

    Integrate Inspinia Admin template - Using Laravel mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,22 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js/')
+	.combine([
+		'inspinia-assets/js/jquery-3.1.1.min.js',
+		'inspinia-assets/js/bootstrap.min.js',
+		'inspinia-assets/js/plugins/metisMenu/jquery.metisMenu.js',
+		'inspinia-assets/js/plugins/slimscroll/jquery.slimscroll.min.js',
+		'inspinia-assets/js/inspinia.js',
+		'inspinia-assets/js/plugins/pace/pace.min.js',
+		'inspinia-assets/js/plugins/wow/wow.min.js',
+	], 'public/js/app.js')
+
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .combine([
+	     'inspinia-assets/css/bootstrap.min.css'
+	    ,'inspinia-assets/css/animate.css'
+	    ,'inspinia-assets/font-awesome/css/font-awesome.css'
+	    ,'inspinia-assets/css/style.css'
+   	], 'public/css/app.css')
+   .version();
